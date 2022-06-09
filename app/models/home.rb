84cc0f804aa6de0a30f38f8fe80e7d6aab9fc4b6
Ruslan Khait <rusty6916@gmail.com>
2022-06-09 16:20:53 -0400

    fixed the relationship between user and homes, there is no relationship. OMGGGG

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,6 +1,6 @@
 class Home < ApplicationRecord
     has_many :reservations 
     has_many :users, through: :reservations 
-    belongs_to :owner 
+ 
     has_many :images
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :reservations
     has_many :homes, through: :reservations
-    has_many :homes
+    #has_many :houses
 end
