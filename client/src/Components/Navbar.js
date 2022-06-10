e3cf37d084b20a2c3da3d64868595eb9ed3b0a35
Ruslan Khait <rusty6916@gmail.com>
2022-06-10 17:21:40 -0400

    design a nav for  HomePage

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import { Navlink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-    return(<div></div>)
+    return(
+    <div className='nav-menu'>
+        <NavLink to="/">
+            <div className="nav-home">
+               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAayO5E7i8zdsHjkiU_hRPV-FQBhoISFteuQ&usqp=CAU"/>
+               <span className="nav-fake-business">Vacation Spot</span>
+            </div>
+        </NavLink>
+        
+    </div>)
 }
 
 export default Navbar
