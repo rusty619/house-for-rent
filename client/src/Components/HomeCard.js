b398b55449ff5ee523136ceb2c17fc8a9efc77a1
Ruslan Khait <rusty6916@gmail.com>
2022-06-12 19:09:04 -0400

    fixed up  homecard

--- client/src/Components/HomeCard.js
+++ client/src/Components/HomeCard.js
@@ -1,11 +1,10 @@
 import React from "react"
-
+import {Link} from "react-router-dom"
 const HomeCard = ({home}) =>{
-    const {title, price,images,id} = home
-
+    const {title, price,images,id} = home    
     return(
     <div className="home-item">
-        <img src={images[0].img} className="home-img"/>
+        <Link to={`home/${id}`}><img src={images[0].img} className="home-img"/></Link>
         <p className="home-text">{title}</p>
         <p className="home-text">Price: ${price}</p>
     </div>)
