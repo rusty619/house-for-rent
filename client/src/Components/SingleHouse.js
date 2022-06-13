da216ef7583503596df9a924edf7a323261652b4
Ruslan Khait <rusty6916@gmail.com>
2022-06-13 10:54:45 -0400

    Fetching into a single house works as well as access to the images works

--- client/src/Components/SingleHouse.js
+++ client/src/Components/SingleHouse.js
@@ -1,10 +1,40 @@
-import React from "react"
+import React,{useState, useEffect} from "react"
 import {useParams} from "react-router-dom"
 
 const SingleHouse = () => {
     const {id} = useParams()
     console.log(id)
-    return(<div>id: {id}</div>)
+
+    const [homeData,setHomeData] = useState([])
+
+    const request = async () => {
+        let req = await fetch(`/homes/${id}`)
+        let res = await req.json()
+        setHomeData(res)
+    }
+
+    useEffect(()=>{
+        request()
+    },[])  
+
+    console.log(homeData)
+    console.log(homeData.images && homeData.images.length)
+    let len = homeData.images && homeData.images.length
+    console.log("Length is ",len)
+    return(
+    <div>
+        id: {id}
+        <p>{homeData.title}</p>
+        <div className="single-house-img">
+        {
+            homeData.images && homeData.images.map((image) => {
+                return(
+                    <img src={image.img} />
+                )
+            })
+        }
+        </div>
+    </div>)
 }
 
 export default SingleHouse
