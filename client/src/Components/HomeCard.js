3ab4320d3a1d5cc5ef06e62c6d14134836cc1938
Ruslan Khait <rusty6916@gmail.com>
2022-06-13 10:23:58 -0400

    Now user can also click the text to get into the singlePage for the house they clicked

--- client/src/Components/HomeCard.js
+++ client/src/Components/HomeCard.js
@@ -4,9 +4,10 @@ const HomeCard = ({home}) =>{
     const {title, price,images,id} = home    
     return(
     <div className="home-item">
-        <Link to={`home/${id}`}><img src={images[0].img} className="home-img"/></Link>
-        <p className="home-text">{title}</p>
-        <p className="home-text">Price: ${price}</p>
+        <Link to={`home/${id}`}><img src={images[0].img} className="home-img"/>
+          <p className="home-text">{title}</p>
+          <p className="home-text">Price: ${price}</p>
+        </Link>
     </div>)
 }
 
