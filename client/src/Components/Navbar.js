eb33f0837ff14342f9b67a8e9ff9780e3733db66
Ruslan Khait <rusty6916@gmail.com>
2022-06-14 14:45:20 -0400

    changed company name to BlueCover, changed it on navbar components

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         <NavLink to="/">
             <div className="nav-home">
                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAayO5E7i8zdsHjkiU_hRPV-FQBhoISFteuQ&usqp=CAU"/>
-               <span className="nav-fake-business">Vacation Spot</span>
+               <span className="nav-fake-business">BlueCover</span>
             </div>
         </NavLink>
         
