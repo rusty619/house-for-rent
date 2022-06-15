946e3400081d220bd5f0e6c4913588b43a99d027
Ruslan Khait <rusty6916@gmail.com>
2022-06-15 17:54:59 -0400

    Donw with ThingsToKnow

--- client/src/Components/ThingsToKnow.js
+++ client/src/Components/ThingsToKnow.js
@@ -4,6 +4,46 @@ const ThingsToKnow = () => {
     return(
     <div className="things-to-know">
         <h1 className="things-to-know-title">Things to know</h1>
+        <div className="things-to-know-rows">
+            <div className="things-to-know-first-column">
+                <p className="things-to-know-column-header">House rules</p>
+                <div className="things-to-know-icon-with-text">
+                    <i class="fa-solid fa-clock"></i>
+                    <p className="things-to-know-column-text">Check-in: After 3:00 PM </p>
+                </div>
+                <div className="things-to-know-icon-with-text">
+                    <i class="fa-solid fa-clock"></i>
+                    <p className="things-to-know-column-text">Checkout: 12:00 PM </p>
+                </div>
+                <div className="things-to-know-icon-with-text">
+                    <i class="fa-solid fa-door-closed"></i>
+                    <p className="things-to-know-column-text">Self check-in with smart lock, keypad </p>
+                </div>
+                <div className="things-to-know-icon-with-text">
+                    <i class="fa-solid fa-paw"></i>
+                    <p className="things-to-know-column-text">Pets are allowed </p>
+                </div>
+                <div className="things-to-know-icon-with-text">
+                    <i class="fa-solid fa-beer-mug-empty"></i>
+                    <p className="things-to-know-column-text">Yes alcohol will be given to tenants as gifts</p>
+                </div>
+            </div>
+            <div className="things-to-know-second-column">
+                <p className="things-to-know-column-header">Health & safety</p>
+                <div className="things-to-know-icon-with-text">
+                    <i class="fa-solid fa-head-side-mask"></i>
+                    <p className="things-to-know-column-text">BlueCover's COVID-19 safety practices apply </p>
+                </div>
+                <div className="things-to-know-icon-with-text">
+                    <i class="fa-solid fa-smog"></i>
+                    <p className="things-to-know-column-text">Carbon monoxide alarm </p>
+                </div>
+                <div className="things-to-know-icon-with-text">
+                    <i class="fa-solid fa-bong"></i>
+                    <p className="things-to-know-column-text">Smoke alarm </p>
+                </div>
+            </div>
+        </div>
     </div>)
 }
 
