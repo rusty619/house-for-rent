9887ba30846fcff1a4cb447c018fd14f9fb72073
Ruslan Khait <rusty6916@gmail.com>
2022-06-14 21:16:10 -0400

    fixed up blue-cover-desc css postion of the text

--- client/src/Components/SleepArea.js
+++ client/src/Components/SleepArea.js
@@ -2,18 +2,6 @@ import React from 'react'
 
 const SleepArea = ({homeData}) => {
 
-    const homeImageDataType = () =>{
-            homeData.images && homeData.images.map((image) => {
-                // if( image.room_type === 'bedroom'){
-                //     // return(
-                //     //     <div><img src={image.img} className="sleep-area-img"/></div>
-                //     // )
-                //     console.log(image)
-                // }
-                return (<p>image.room_type</p>)
-            })
-    }
-
     return(
     <div className='sleep-area'>
         <p className="sleep-area-text">Where you'll sleep</p>
