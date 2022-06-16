5e55cebd0cc8ca3a44f78611cbd338e175d75588
Ruslan Khait <rusty6916@gmail.com>
2022-06-16 15:14:30 -0400

    Finished Footer Component

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           <SingleHouse />
         </Route>
       </Switch>
-
+      <Footer />
     </div>
   );
 }
--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
-    return(<div></div>)
+    return(
+    <div className="footer">
+        <div className="footer-left">
+            <p className='footer-left-copyright'>© 2022 Airbnb, Inc.</p>
+            <p className="footer-left-text">Privacy</p>
+            <p className="footer-left-text">Terms</p>
+            <p className="footer-left-text">Sitemap</p>
+        </div>
+        <div className="footer-right">
+            <i class="fa-solid fa-globe"></i>
+            <p class="footer-right-text">English</p>
+            <p class="footer-right-text">(US)</p>
+            <p class="footer-right-text">$</p>
+            <p class="footer-right-text">USD</p>
+            <a href="https://www.youtube.com/watch?v=X9m15uyVpIs" class="footer-right-a"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="https://mobile.twitter.com/GaryVee_Quotes"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://www.instagram.com/garyvee/?hl=en"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+    </div>)
 }
 
 export default Footer;
