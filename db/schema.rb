c159c5a676fa1c8ec4e9e558f932cca365fbd4e4
Ruslan Khait <rusty6916@gmail.com>
2022-06-16 19:06:27 -0400

    added a new column profile_img into users table and added a serializer into user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_200113) do
+ActiveRecord::Schema.define(version: 2022_06_16_225429) do
 
   create_table "homes", force: :cascade do |t|
     t.string "home_type"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_06_09_200113) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "profile_img"
   end
 
 end
