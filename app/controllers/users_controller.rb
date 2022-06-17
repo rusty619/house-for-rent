e32b224663b60f23f577221fa7e4080b31b5d139
Ruslan Khait <rusty6916@gmail.com>
2022-06-16 20:24:53 -0400

    added profile_img into user_params in the user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params 
-        params.permit(:name)
+        params.permit(:name, :profile_img)
     end
 
     def render_unprocessable_entity_response(exception) 
