42808fd2c23eb214dffba0d5a93186f404febcef
Ruslan Khait <rusty6916@gmail.com>
2022-06-20 16:21:08 -0400

    finished phase 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
-import React,{useState, useEffect} from "react"
+import React,{useState, useEffect, Component} from "react"
 import {BrowserRoutes, Route, Router, Switch} from "react-router-dom"
+import axios from 'axios'
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import SingleHouse from "./Components/SingleHouse";
 import Footer from "./Components/Footer";
 import './App.css';
 
-function App() {
+function App(){
+
+   const [isLoggedIn,setLoggedIn] = useState(false)
+   const [user, setUser] = useState({})
+
+   const loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in',
+    {withCredentials: true})
+    .then(res => {
+      if(res.data.logged_In){
+        this.handleLogin(res)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors: ', error))
+   }
+
+   handleLogin = (data) => {
+     setLoggedIn(true)
+     setUser(data.user)
+   }
+
+   handleLogout = () => {
+    setLoggedIn(false)
+    setUser({})
+   }
+
+  componentDidMount = () => {
+    this.loginStatus()
+   }
 
   return (
     <div className="App">
@@ -18,6 +49,8 @@ function App() {
         <Route path="/home/:id" exact>
           <SingleHouse />
         </Route>
+        <Route path="/login" exact></Route>
+        <Route path="/signup" exact></Route>
       </Switch>
       <Footer />
     </div>
